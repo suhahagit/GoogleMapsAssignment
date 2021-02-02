870250789f91af3d206744bad3395a3a50557736
Your username <suhaha.git@gmail.com>
2021-02-02 22:10:01 +0200

    final assignement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const IP_DATA_KEY = 'dcb19aafdd3accd0ca61b014f91a6cc5888d57f4f703a9758da3751e'
+const GOOGLE_KEY = 'AIzaSyCdQymwSuF0P6Ee-ffX0ZtWjpJdpaT5eLk'
 
 function App() {
+
+  const [location, setLocation] = useState({})
+
+  useEffect(() => {
+    async function fetchData() {
+      const ipData = await axios.get(`https://api.ipdata.co?api-key=${IP_DATA_KEY}`)
+      setLocation(ipData.data)
+    }
+    fetchData()
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>City Name: {location.city}</p>
+      <p>Country Name: {location.country_name}</p>
+      <iframe title="googleFrame"
+        width="600"
+        height="450"
+        frameBorder="0" 
+        src={`https://www.google.com/maps/embed/v1/place?key=${GOOGLE_KEY}
+    &q=${location.city}, ${location.country_name}`} allowFullScreen>
+      </iframe>
     </div>
   );
 }
